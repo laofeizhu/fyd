068f326a75f967cef22722d58562c48daa68601b
laofeizhu <bjrebeccali@gmail.com>
2019-03-13 21:03:02 -0400

    retrive univs info at server start

--- src/routes/univs.js
+++ src/routes/univs.js
@@ -1,5 +1,9 @@
 //Here is where we declare the modules and shit we will need
 const express = require('express')
+const { Univ } = require('../database/models')
+
+const trunks = require('trunks-log')
+const log = new trunks('UNIVS')
 
 //import the controllers and middleware
 const { univsController } = require('../controllers/index')
@@ -8,8 +12,16 @@ const { catchErrors } = require('../middleware/error-handler')
 //set up the router
 const router = express.Router()
 
+let univs
+Univ.find().exec().then(univsInDb => {
+    log.success('Retrieved all {} univs', univsInDb.length)
+    univs = univsInDb
+}).catch(err => {
+  log.error(err, 'Could not retrieve univs: {}', err.message)
+})
+
 //get all univs
-router.get('/', catchErrors(univsController.index))
+router.get('/', (req, res) => { res.json({univs})} )
 
 //make a new boy
 router.post('/', catchErrors(univsController.store))
