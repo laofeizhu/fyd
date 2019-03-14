61415577c5818e91a44c22f07d32dd9206dd7f05
laofeizhu <bjrebeccali@gmail.com>
2019-03-14 00:40:34 -0400

    db cache, reorganize data on serverside, get unit price for renting

--- src/routes/univs.js
+++ src/routes/univs.js
@@ -9,19 +9,39 @@ const log = new trunks('UNIVS')
 const { univsController } = require('../controllers/index')
 const { catchErrors } = require('../middleware/error-handler')
 
+const { parseUnivInfo } = require('../util/parse-univs-info')
+
+const fs = require("fs")
+
 //set up the router
 const router = express.Router()
 
-let univs
-Univ.find().exec().then(univsInDb => {
+var univs
+var parsedUnivs
+const readFromCache = true
+if (readFromCache) {
+  fs.readFile('univCache', (err, data) => {
+    univs = JSON.parse(data)
+    univs = univs.slice(0, 2)
+    parsedUnivs = univs.map(parseUnivInfo)
+  })
+} else {
+  Univ.find().exec().then(univsInDb => {
     log.success('Retrieved all {} univs', univsInDb.length)
     univs = univsInDb
-}).catch(err => {
-  log.error(err, 'Could not retrieve univs: {}', err.message)
-})
+    fs.writeFile('univCache', JSON.stringify(univsInDb), err => {
+      if (err) throw err;
+      log.info("univs cached!")
+    })
+  }).catch(err => {
+    log.error(err, 'Could not retrieve univs: {}', err.message)
+  })
+}
 
 //get all univs
-router.get('/', (req, res) => { res.json({univs})} )
+router.get('/', (req, res) => {
+  res.json({univs: parsedUnivs})
+})
 
 //make a new boy
 router.post('/', catchErrors(univsController.store))
