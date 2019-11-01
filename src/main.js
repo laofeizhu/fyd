6a27427b623ff838e078f0d1f7d3d240b8cbd8c8
laofeizhu <laofeizhu@github.com>
2019-10-31 21:59:56 -0400

    add fonts back

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.use(Element)
 
